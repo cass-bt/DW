72f8086c8afcd9d8d0c8968264c7165d79c5e59a
Cassio Bastos <cassio.contato20@gmail.com>
2024-09-02 19:19:46 -0700

    commit

--- backend-DW/Utilitaries/routes/number.js
+++ backend-DW/Utilitaries/routes/number.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const numberController = require('../controllers/numberController');
 
-// GET /number/:action
 router.get('/:action', numberController.processNumber);
 
 module.exports = router;
